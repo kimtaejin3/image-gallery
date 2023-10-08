9e3113222dc9d8a3658de4968e2cc762cab06643
Taejin Kim <rlaxowls1316@naver.com>
2023-10-08 12:18:36 +0900

    Zod & Fetching Images & Remote Images

---
+++ src/lib/fetchImages.ts
@@ -0,0 +1,34 @@
+import type { ImagesResults } from "@/models/Images";
+import { ImagesSchemaWithPhotos } from "@/models/Images";
+import env from "./env";
+
+export default async function fetchImages(
+  url: string
+): Promise<ImagesResults | undefined> {
+  try {
+    const res = await fetch(url, {
+      headers: {
+        Authorization: env.PEXELS_API_KEY,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error("Fetch Images error!");
+    }
+
+    const imaegsResults = await res.json();
+    console.log("==");
+    console.log(imaegsResults);
+    console.log("==");
+    //parse data with zod schema
+    const parsedData = ImagesSchemaWithPhotos.parse(imaegsResults);
+
+    if (parsedData.total_results === 0) return undefined;
+
+    return parsedData;
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e);
+    }
+  }
+}
